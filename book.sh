0d7673fce7a38b2c401e41da0793b4312f75d5c1
ysykzheng <1020680508@qq.com>
2023-05-11 13:04:05 +0800

    book output

---
+++ book.sh
@@ -0,0 +1,7 @@
+npm install
+npx honkit epub ./ leadership-principles-for-the-workplace-timeless-strategies-for-leading-with-purpose-and-vision.epub
+
+ebook-convert leadership-principles-for-the-workplace-timeless-strategies-for-leading-with-purpose-and-vision.epub leadership-principles-for-the-workplace-timeless-strategies-for-leading-with-purpose-and-vision.pdf --no-chapters-in-toc --chapter-mark=none --subset-embedded-fonts  --pdf-page-numbers --disable-markup-chapter-headings --paper-size a5 --pdf-page-margin-bottom 36 --pdf-page-margin-left 36 --pdf-page-margin-right 36 --pdf-page-margin-top 36 --extra-css page_styles.css
+
+# brew install pdftk-java
+java -jar "/usr/local/Cellar/pdftk-java/3.3.3/libexec/pdftk-all.jar" leadership-principles-for-the-workplace-timeless-strategies-for-leading-with-purpose-and-vision.pdf cat 2-end output leadership-principles-for-the-workplace-timeless-strategies-for-leading-with-purpose-and-vision-FINAL.pdf
